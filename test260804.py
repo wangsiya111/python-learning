23419a15a6c8419902be7127fca4698197c730ae
王思雅 <2459509931@qq.com>
2026-09-14 22:14:20 +0800

    Add files via upload

---
+++ test260804.py
@@ -0,0 +1,32 @@
+# 我将认真学习黑马程序员的python视频
+"""
+今天是第一天日期为2026年9月14日,今天学习内容为变量和数据类型
+变量相当于一个盒子来储存数据用的,特点是可变
+
+加油
+"""
+
+# 变量 变量名=变量值
+money =50
+print("当前钱包余额为： ",money)
+money =money - 5
+print("购买冰淇淋花费了： ",5)
+print("当前钱包余额为： ",money)
+print("购买了可乐花费: ",10)
+money =money -10
+print("当前钱包余额为： ",money)
+
+# 数据类型：整型、浮点型、字符型
+# 1.type(变量)就可以输出类型
+# 2.变量没有类型
+print(type(100),type(1.55),type("你好"))
+int_type=type(100)
+print(int_type)
+str_type=type("我喜欢你")
+print(str_type)
+float_type=type(5.20)
+print(float_type)
+apple=100
+apple_type=type(apple)
+print(apple_type)
+#中午2个小时学习结束，晚上我将从数据类型转换开始！！！
